2aa87ef64f8571ccb12350087d05ae87629fd40e
Badr Labloul <badr2labloul@gmail.com>
2026-08-01 00:06:41 -0500

    Styling Cards

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -3,10 +3,10 @@ import {defaultClothingItems} from "../../utils/constants.js";
 
 function ItemCard({ item }) {
   return (
-    <div className="item-card" key={item._id}>
-      <h2 className="item-card__title">{item.name}</h2>
-      <img src={item.link} alt={item.name} className="item-card__image" />
-    </div>
+    <li className="card" key={item._id}>
+      <h2 className="card__name">{item.name}</h2>
+      <img src={item.link} alt={item.name} className="card__image" />
+    </li>
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,7 +12,7 @@ function Main({ weatherData }) {
       <section className="cards">
         <p className="cards__text">Today is 75 &deg; F / You may want to wear:</p>
         <ul className="cards__list">
-          {defaultClothingItems.filter((item) => {return item.weather === weatherData.condition}).map((item) => (
+          {defaultClothingItems/*.filter((item) => {return item.weather === weatherData.condition})*/.map((item) => (
             <ItemCard key={item._id} item={item} />
           ))}
         </ul>
