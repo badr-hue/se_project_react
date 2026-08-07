8e93b58fe5e351a66b3553bae01a067fc64e5a41
Badr Labloul <badr2labloul@gmail.com>
2026-08-07 11:53:19 -0500

    Project Sprint 10

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,1 +1,9 @@
 import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">© 2024 WeatherWear</p>
+    </footer>
+  );
+}
